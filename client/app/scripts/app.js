19920e7dcacdc0991d1affc2a24e35d62864dc43
maxdavinci <maxdavinci@gmail.com>
2016-02-14 00:53:38 -0500

    Express and Angular Scaffolding

---
+++ client/app/scripts/app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+var clientApp = angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+   // 'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap',
+    'ui.router'
+  ]);
+clientApp.config( function($stateProvider) {
+    //$urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state('index', {
+        url: "/",
+        views: {
+          'carousel': {
+            templateUrl: 'views/carousel.html',
+            controller: 'CarouselCtrl'
+            //controllerAs: 'carousel'
+            //template: "index.viewA" 
+          },
+          'schedule': {
+            templateUrl: 'views/schedule.html',
+            controller: 'ScheduleCtrl',
+            controllerAs: 'schedule'
+          },
+          'ticker': {
+            templateUrl: 'views/ticker.html',
+            controller: 'TickerCtrl',
+            controllerAs: 'ticker'},
+        }
+      })
+      .state('about', {
+        url: "/about",
+        views: {
+          'carousel': {
+            //templateUrl: 'views/main.html',
+            //controller: 'MainCtrl',
+            //controllerAs: 'main'
+            template: "about.viewA" 
+          },
+          'schedule': {template: "about.viewB"},
+          'ticker': {},
+        }
+      })
+      .state('contact', {
+        url: "/contact",
+        views: {
+          'carousel': {
+            //templateUrl: 'views/main.html',
+            //controller: 'MainCtrl',
+            //controllerAs: 'main'
+            template: "contact.viewA" 
+          },
+          'schedule': {template: "contact.viewB"},
+          'ticker': {},
+        }
+      });
+  }
+);
+/*.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }*/
+  
